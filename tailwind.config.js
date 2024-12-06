95cdd8974fc27b46f051b67064a67ad70398f777
FaheemPechuho <117772555+FaheemPechuho@users.noreply.github.com>
2024-12-06 18:12:20 +0500

    project initialize

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+ theme: {
+    colors: {
+      primary: "#b62721", 
+      secondary: "#f5e5e5"
+    },
+   }, 
+  plugins: [],
+}
+
